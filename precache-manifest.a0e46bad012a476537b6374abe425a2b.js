0875ae9241aa5b0715b310918b2b5927c7b182c3
Alex Ackerman <alexp.ackerman@gmail.com>
2019-09-09 18:39:33 -0500

    Updates

---
+++ precache-manifest.a0e46bad012a476537b6374abe425a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45a3004c2e1ef4cc062afb5c23d095a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b9c7b9a7bc3f9cfdefe",
+    "url": "/static/css/main.dc93d613.chunk.css"
+  },
+  {
+    "revision": "c50fe8ca1796e1a06de5",
+    "url": "/static/js/2.aa5e375a.chunk.js"
+  },
+  {
+    "revision": "8b9c7b9a7bc3f9cfdefe",
+    "url": "/static/js/main.63e1ea3d.chunk.js"
+  },
+  {
+    "revision": "c18dc75b4d354d47d743",
+    "url": "/static/js/runtime~main.ee3c83b0.js"
+  }
+]);

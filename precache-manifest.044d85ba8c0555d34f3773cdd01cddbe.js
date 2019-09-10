84157d7fdf56a7fa19f65560b7f992ddb0a0ab9c
Alex Ackerman <alexp.ackerman@gmail.com>
2019-09-09 22:17:05 -0500

    Updates

---
+++ precache-manifest.044d85ba8c0555d34f3773cdd01cddbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590c046add6dc6d05a7ee2f3261fa1c2",
+    "url": "/ScribeAR.github.io/index.html"
+  },
+  {
+    "revision": "ec03c7e00b28a5b5f798",
+    "url": "/ScribeAR.github.io/static/css/main.dc93d613.chunk.css"
+  },
+  {
+    "revision": "d3b416b52e558c1d8215",
+    "url": "/ScribeAR.github.io/static/js/2.997d0f9a.chunk.js"
+  },
+  {
+    "revision": "ec03c7e00b28a5b5f798",
+    "url": "/ScribeAR.github.io/static/js/main.cfbe158f.chunk.js"
+  },
+  {
+    "revision": "c03fd9e925687451093d",
+    "url": "/ScribeAR.github.io/static/js/runtime~main.653a97ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f025c9b1b7f2bf408dafbde635284648.js"
+  "/ScribeAR.github.io/precache-manifest.044d85ba8c0555d34f3773cdd01cddbe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ScribeAR.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

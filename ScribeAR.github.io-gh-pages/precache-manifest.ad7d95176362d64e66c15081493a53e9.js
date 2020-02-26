26e36d13fa641a0ff5e37ce5402c433ad4866148
Will Foster <42978593+WilliamFoster3@users.noreply.github.com>
2020-02-26 14:45:04 -0600

    Adding gh-pages branch to Master

---
+++ ScribeAR.github.io-gh-pages/precache-manifest.ad7d95176362d64e66c15081493a53e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5f43c5d2dc76946a0eebc345b60501",
+    "url": "/ScribeAR.github.io/index.html"
+  },
+  {
+    "revision": "0ebc30e9f811911ca430",
+    "url": "/ScribeAR.github.io/static/css/main.b6aec3f8.chunk.css"
+  },
+  {
+    "revision": "82381f8522b10a2f6b35",
+    "url": "/ScribeAR.github.io/static/js/2.15587fa7.chunk.js"
+  },
+  {
+    "revision": "b9f9759b9b1285134e0534121ec7f9b0",
+    "url": "/ScribeAR.github.io/static/js/2.15587fa7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ebc30e9f811911ca430",
+    "url": "/ScribeAR.github.io/static/js/main.2775d2db.chunk.js"
+  },
+  {
+    "revision": "9c8e082ebc3cd6952b96",
+    "url": "/ScribeAR.github.io/static/js/runtime-main.8965f101.js"
+  }
+]);

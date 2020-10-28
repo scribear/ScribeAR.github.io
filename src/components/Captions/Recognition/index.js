aebba34f524a8d84be9e7f1299f137fc030c230f
Will Foster <42978593+WilliamFoster3@users.noreply.github.com>
2020-10-28 05:32:26 -0500

    Add files via upload

--- src/components/Captions/Recognition/index.js
+++ src/components/Captions/Recognition/index.js
@@ -1,315 +1,87 @@
-import React from 'react'
-import store from '../../../store/';
-import $ from 'jquery';
-import ScrollButton from 'react-scroll-button'
-import ScrollToBottom from 'react-scroll-to-bottom';
+import React, {useState} from 'react'
+import { useSelector , useDispatch} from 'react-redux'
+import './index.css'
+import Recognition from './Recognition'
 
-import { flip_recording, flip_switch_to_azure,
-         flip_switchMenus, flip_entered_key,
-         flip_correct_azureKey, flip_on_webspeech,
-         flip_check_azureKey,
-         flip_entered_region } from '../../../redux/actions'
-
-import * as SpeechSDK from 'microsoft-cognitiveservices-speech-sdk'
-import AzureKey from '../../AzureTopSpace/AzureOptions/Key';
-import { isPureish } from '@babel/types';
-import swal from 'sweetalert';
-import {useSelector, connect, useDispatch} from 'react-redux'
-import {bindActionCreators} from "redux"
+import { Button, IconButton } from "@material-ui/core"
+import store from '../../store'
+import mytheme from '../newDrawer/theme'
+import Extender from './Extender'
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import {ThemeProvider} from "@material-ui/core/styles";
+import {
+     increment_numLines,
+     decrement_numLines,
+} from '../../redux/actions'
 
 
-var onWebspeech = true;
-//Azure Variables
-var reco = null;
-var key = "empty"
-var regionOption = "empty"
-var lang = "en-US"
-var targetLang = "en"
-var azureSpeechConfig = null;
-const azureAudioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+export default function Captions(props) {
+     const lineWidth = useSelector((state) => state.lineWidth)
+     const numLines = useSelector((state) => state.numLines)
+     const recording = useSelector((state) => state.recording)
 
+     const [maxHeight, setMaxHeight] = useState(64)
+     const [visible, setVisible] = useState(false)
 
-//Webspeech Variables
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-const recognition = new SpeechRecognition()
-recognition.lang = 'en-US'
-recognition.continuous = false
-recognition.interimResults = true
+     const onWebspeech = useSelector((state) => state.onWebspeech)
+     const correctAzureKey = useSelector((state) => state.correctAzureKey)
+     const checkAzureKey= useSelector((state) => state.checkAzureKey)
 
-const mapDispatchToProps = {
-  flip_entered_region,
-  flip_entered_key,
-  flip_correct_azureKey,
-  flip_on_webspeech,
-}
+     var paddingString = (11 - lineWidth) * 3 + 'vw'
+     var h = numLines + 'vh'
+     var resH = (43 - numLines) + 'vh'
+     var sz = props.textSize
 
-export class Recognition extends React.PureComponent {
-    constructor(props) {
-        super(props)
-        this.state = {
-           line: '',
-           targetID: 'curr',
-        }
-
-        this.azureAppendLine = this.azureAppendLine.bind(this)
-        this.azureStart = this.azureStart.bind(this)
-        this.azureStop = this.azureStop.bind(this)
-        this.webspeechAppendLine = this.webspeechAppendLine.bind(this)
-        this.webspeechStart = this.webspeechStart.bind(this)
-        this.webspeechStop = this.webspeechStop.bind(this)
-
-    }
-
-    componentDidMount() {
-       this.webspeechStart()
-    }
-
-    // Global state 'recording' is passed as a prop. componentDidUpdate is invoked
-    // when props change, therefore also when 'recording' changes.
-    componentDidUpdate(prevProps, prevState) {
-         if (this.props.wantsWebspeech === false && store.isSuccessReducer == "success") {
-           this.onWebspeech = false;
-           if (store.webspeechIsStart === "true") {
-             this.webspeechStop()
-           }
-           if (this.props.isRecording) {
-             this.azureStart()
-           } else {
-             this.azureStop()
-           }
-         } else if (this.props.shouldCheck === true && store.isSuccessReducer != "success") {
-           this.onWebspeech = false;
-           if (store.webspeechIsStart === "true") {
-             this.webspeechStop()
-           }
-           this.azureStart()
-         } else {
-           this.onWebspeech = true;
-           if (store.azureIsStart === "true") {
-             this.azureStop()
-           }
-           if (prevProps.isRecording === this.props.isRecording && prevProps.wantsWebspeech === this.props.wantsWebspeech)
-              return
-           if (this.props.isRecording)
-              this.webspeechStart()
-           else {
-              this.webspeechStop()
-         }
+     const DisTop = () => {
+       if (document.getElementById("captionsSpace").scrollTop >= maxHeight) {
+          setMaxHeight(document.getElementById("captionsSpace").scrollTop);
        }
-    }
-
-    downloadTxtFile = () => {
-      const element = document.createElement("a");
-      var results = [];
-      results.push("transcript History \n\n\n\n");
-      var searchEles = document.getElementById("out");
-      results.push(searchEles.innerHTML);
-      const file = new Blob([results], {type: 'text/plain;charset=utf-8'});
-      element.href = URL.createObjectURL(file);
-      element.download = "Script.txt";
-      document.body.appendChild(element);
-      element.click();
-    }
-
-
-    azureAppendLine(str) {
-     const capts = document.getElementById('captionsSpace')
-     const out = document.getElementById('out')
-     var isScrolledToBottom = capts.scrollHeight - capts.clientHeight <= capts.scrollTop + 1
-     var div = document.createElement('div') // create new div
-     div.textContent = str // set new div's text to the updated current line
-     out.appendChild(div) // add the new div to the document inside 'out' element
-     this.setState({ line: '' }) // reset line
-     if (isScrolledToBottom)
-          capts.scrollTop = capts.scrollHeight - capts.clientHeight // scroll to bottom
-}
-
-    azureStart() {
-        store.azureIsStart = "true";
-        if (store.isSuccessReducer != 'success') {
-          store.isSuccessReducer = 'inProgress';
-        if (store.azureKeyReducer == undefined || store.azureRegionOptionsReducer == undefined || store.azureKeyReducer == '' || store.azureRegionOptionsReducer == '') {
-          store.azureKeyReducer = 'empty'
-          store.azureRegionOptionsReducer = 'empty'
-        } else {
-          key = store.azureKeyReducer;
-          regionOption = store.azureRegionOptionsReducer;
-        }
-      }
-          azureSpeechConfig = SpeechSDK.SpeechTranslationConfig.fromSubscription(key, regionOption);
-          lang = store.currentLanguageReducer;
-          targetLang = store.targetLanguageReducer;
-          azureSpeechConfig.speechRecognitionLanguage = lang;
-          azureSpeechConfig.addTargetLanguage(targetLang);
-          //allow all profanity (raw);
-          azureSpeechConfig.setProfanity(2);
-
-          reco = new SpeechSDK.TranslationRecognizer(azureSpeechConfig, azureAudioConfig);
-
-        var out = document.getElementById('out');
-        var lastRecognized = out.innerHTML;
-        reco.sessionStarted = function(s,e) {
-          if (store.isSuccessReducer != 'success') {
-          store.isSuccessReducer = 'success';
-          swal({
-            title: "Success!",
-            text: "Connected to Azure Speech Recognition!",
-            icon: "success",
-            timer: 2000,
-          })
-        }
-      }
-        reco.recognizing = function(s, e) {
-            var language = targetLang;
-            out.innerHTML = lastRecognized + e.result.translations.get(language);
-            const capts = document.getElementById('captionsSpace')
-            capts.scrollTop = capts.scrollHeight - capts.clientHeight // scroll to bottom
-        }
-
-        reco.recognized = function (s,e) {
-            window.console.log("currently azure");
-            var language = targetLang;
-            lastRecognized += e.result.translations.get(language) + "\r\n";
-            out.innerHTML = lastRecognized;
-        }
-        reco.canceled = function (s, e) {
-          if (store.isSuccessReducer != 'success') {
-            store.azureIsStart = "false";
-          store.isSuccessReducer = 'failure'
-          swal({
-              title: "Warning!",
-              text: "Wrong key or region!",
-              icon: "warning",
-            })
-            this.azureStop()
-          }
-        };
-        reco.startContinuousRecognitionAsync();
-    }
-    azureStop() {
-      store.azureIsStart = "false";
-        reco.stopContinuousRecognitionAsync(
-            function() {
-                reco.close();
-                reco = undefined;
-            },
-            function (err) {
-                reco.close();
-                reco = undefined;
-            }
-        );
-    }
-
-
-    //WEBSPEECH WEBSPEECH WEBSPEECH
-
-
-    webspeechStart() {
-      store.webspeechIsStart = "true";
-         recognition.start()
-         // Map the complex recognition result object to a string. You can explore
-         // the full object with console.log(e).
-         recognition.onresult = (e) => {
-           window.console.log("currently webs");
-
-              let words = Array.from(e.results)
-                .map(result => result[0])
-                .map(result => result.transcript)
-                .join('');
-              words = words.charAt(0).toUpperCase() + words.slice(1)
-              if (e.results[0].isFinal) // if line is final
-                   this.webspeechAppendLine(words + '.') // flush buffer
-              else this.updateCurrentLine(words) // update state
-         }
-         // By default, recognition stops when it gets a final result.
-         recognition.onend = recognition.webspeechStart // override this behavior
-    }
-
-    downloadTxtFile = () => {
-      const element = document.createElement("a");
-      var results = [];
-      results.push("transcript History \n\n\n\n");
-      var searchEles = document.getElementById("out").children;
-      console.log(searchEles);
-
-      for(var i = 0; i < searchEles.length; i++) {
-        console.log(searchEles[i].innerHTML[1,searchEles[i].innerHTML.length-2]);
-        results.push(searchEles[i].innerHTML + '\n');
-      }
-
-      // const file = new Blob([document.getElementById('out').value],
-      //             {type: 'text/plain;charset=utf-8'});
-      const file = new Blob([results],
-                  {type: 'text/plain;charset=utf-8'});
-      element.href = URL.createObjectURL(file);
-      element.download = "Script.txt";
-      document.body.appendChild(element);
-      element.click();
-    }
-
-    webspeechStop() {
-      store.webspeechIsStart = "false";
-         recognition.onresult = () => {} // do nothing with results
-         recognition.onend = () => {} // don't restart when ending
-         recognition.stop()
-    }
-
-    updateCurrentLine(str) {
-         const capts = document.getElementById('captionsSpace')
-         var isScrolledToBottom = capts.scrollHeight - capts.clientHeight <= capts.scrollTop + 1
-         this.setState({ line: str })
-         if (isScrolledToBottom)
-              capts.scrollTop = capts.scrollHeight - capts.clientHeight
-    }
-
-    webspeechAppendLine(str) {
-         const capts = document.getElementById('captionsSpace')
-         const out = document.getElementById('out')
-         var isScrolledToBottom = capts.scrollHeight - capts.clientHeight <= capts.scrollTop + 1
-         var div = document.createElement('div') // create new div
-         div.textContent = str // set new div's text to the updated current line
-         out.appendChild(div) // add the new div to the document inside 'out' element
-         this.setState({ line: '' }) // reset line
-         if (isScrolledToBottom)
-              capts.scrollTop = capts.scrollHeight - capts.clientHeight // scroll to bottom
-    }
-
-    scrollBottom() {
-        var element = document.getElementById("curr");
-        element.scrollIntoView({behavior: "smooth"});
-    }
-
-// Webspeech Render
-
- render() {
-
-         // out holds all past lines. curr holds the current line.
-         if (onWebspeech) {
-           return (
-              <div>
-                   <div id='out'></div>
-                   <div id='curr'>{this.state.line}</div>
-              </div>
-         )
+       if (maxHeight - document.getElementById("captionsSpace").scrollTop > 10) {
+         setVisible(true);
        } else {
-         return (
-                   <div>
-                        <div contenteditable = "true" id='out'></div>
-                          <div id='curr'>{this.props.key}</div>
-                   </div>
-              )
+         setVisible(false);
        }
+     };
+      if (visible) {
+        return (
+          <ThemeProvider theme = {mytheme}>
+          <div>
+            <IconButton className="scroll circle-icon"  color='inherit' variant="outlined" onClick= {new Recognition().scrollBottom}>
+                 <DoubleArrowIcon fontSize="large"/>
+          </IconButton>
+            <div onScroll={DisTop} className="captionsSpace" id="captionsSpace"
+             style={{
+               fontSize: sz,
+               height: h,
+               width: "100vw",
+               overflow: "auto",
+               paddingLeft: paddingString,
+               paddingRight: paddingString }}>
+                  <Recognition isRecording={recording} shouldCheck={checkAzureKey} wantsWebspeech={onWebspeech} azureKey={store.azureKeyReducer} azureRegion ={store.azureRegionOptionsReducer}/>
+                </div>
+           </div>
+           </ThemeProvider>
+         )
+      } else {
+        return (
+          <ThemeProvider theme = {mytheme}>
+          <div>
+          <IconButton className="scroll-hidden" color='inherit'  onClick= {new Recognition().scrollBottom}>
+             <DoubleArrowIcon fontSize="large"/>
+          </IconButton>
+            <div onScroll={DisTop} className="captionsSpace" id="captionsSpace"
+             style={{
+               fontSize: sz,
+               height: h,
+               width: "100vw",
+               overflow: "auto",
+               paddingLeft: paddingString,
+               paddingRight: paddingString }}>
+                  <Recognition isRecording={recording} shouldCheck={checkAzureKey} wantsWebspeech={onWebspeech} azureKey={store.azureKeyReducer} azureRegion ={store.azureRegionOptionsReducer}/>
+                </div>
+           </div>
+           </ThemeProvider>
+         )
+      }
 
-  }
-
-   //  render() {
-   //      return (
-   //           <div>
-   //                <div contenteditable = "true" id='out'></div>
-   //                  <div id='azureCurr'>{this.props.key}</div>
-   //           </div>
-   //      )
-   // }
 }
-connect(mapDispatchToProps)(Recognition)
-export default Recognition

3618e0bd23844e0d856e2d87341b6d5b050a2dcc
Jiaming Zhang <qq1104382148@gmail.com>
2020-11-18 14:40:27 -0600

    eslint update

--- src/components/AzureCaptions/index.js
+++ src/components/AzureCaptions/index.js
@@ -6,54 +6,57 @@ import Extender from './Extender'
 import { Button } from "@material-ui/core"
 import ScrollButton from 'react-scroll-button'
 import store from '../../store'
-import { increment_numLines,
-decrement_numLines, flip_correct_azureKey, flip_check_azureKey } from '../../redux/actions'
+import {
+  increment_numLines,
+  decrement_numLines, flip_correct_azureKey, flip_check_azureKey
+} from '../../redux/actions'
 
 
 
 export default function AzureCaptions(props) {
-     const dispatch = useDispatch()
-     const lineWidth = useSelector((state) => state.lineWidth)
-     const numLines = useSelector((state) => state.numLines)
-     const recording = useSelector((state) => state.recordingAzure)
-     const checkAzureKey = useSelector((state) => state.checkAzureKey)
-     const correctAzureKey = useSelector((state) => state.correctAzureKey)
-     // Sloppy styling. Please change.
-     var paddingString = (11 - lineWidth) * 3 + 'vw'
-     var h = numLines + 'vh'
-     var resH = (43 - numLines) + 'vh'
-     var sz = props.textSize
-     if (props.wantWebspeech == true && store.isSuccessReducer == 'success') {
-       store.desiredAPI = 'webspeech';
-       return (
-         <div>
-            <AzureRecognition switchOffAzure = {true} />
-         </div>
-        )
-     } else {
-       store.desiredAPI = 'azure';
-       return ( <div>
-                      <div style = {{
-                           height : resH,
-                           margin : '0.5vh',
-                      }}>
-                      <Extender
-                                          increment={increment_numLines}
-                                          decrement={decrement_numLines}  />
-                      </div>
-                      <Button className="scroll" position="fixed" variant="outlined" onClick= {new AzureRecognition().scrollBottom} color="secondary">Scroll to Bottom</Button>
-                      <div className="captionsSpace" id="captionsSpace"
-                      style={{
-                      fontSize: sz,
-                      height: h,
-                      width: "100vw",
-                      overflow: "auto",
-                      paddingLeft: paddingString,
-                      paddingRight: paddingString }}>
+  const dispatch = useDispatch()
+  const lineWidth = useSelector((state) => state.lineWidth)
+  const numLines = useSelector((state) => state.numLines)
+  const recording = useSelector((state) => state.recordingAzure)
+  const checkAzureKey = useSelector((state) => state.checkAzureKey)
+  const correctAzureKey = useSelector((state) => state.correctAzureKey)
+  // Sloppy styling. Please change.
+  var paddingString = (11 - lineWidth) * 3 + 'vw'
+  var h = numLines + 'vh'
+  var resH = (43 - numLines) + 'vh'
+  var sz = props.textSize
+  if (props.wantWebspeech == true && store.isSuccessReducer == 'success') {
+    store.desiredAPI = 'webspeech';
+    return (
+      <div>
+        <AzureRecognition switchOffAzure={true} />
+      </div>
+    )
+  } else {
+    store.desiredAPI = 'azure';
+    return (<div>
+      <div style={{
+        height: resH,
+        margin: '0.5vh',
+      }}>
+        <Extender
+          increment={increment_numLines}
+          decrement={decrement_numLines} />
+      </div>
+      <Button className="scroll" position="fixed" variant="outlined" onClick={new AzureRecognition().scrollBottom} color="secondary">Scroll to Bottom</Button>
+      <div className="captionsSpace" id="captionsSpace"
+        style={{
+          fontSize: sz,
+          height: h,
+          width: "100vw",
+          overflow: "auto",
+          paddingLeft: paddingString,
+          paddingRight: paddingString
+        }}>
 
-                           <AzureRecognition isRecording = {recording} isCrorrect = {correctAzureKey} checkKey = {checkAzureKey}
-                            key = {store.azureKeyReducer} region = {store.azureRegionOptionsReducer} switchOffAzure = {false} />
-                      </div>
-                </div> )
-        }
+        <AzureRecognition isRecording={recording} isCrorrect={correctAzureKey} checkKey={checkAzureKey}
+          key={store.azureKeyReducer} region={store.azureRegionOptionsReducer} switchOffAzure={false} />
+      </div>
+    </div>)
+  }
 }
--- src/components/TopSpace/Options/PlusMinus/PlusMinus.js
+++ src/components/TopSpace/Options/PlusMinus/PlusMinus.js
@@ -5,21 +5,21 @@ import ToggleButton from "../../../ToggleButton"
 
 
 export default function PlusMinus(props) {
-     const setting = useSelector(props.setting) // Get current value of the setting.
-     // useDispatch returns the state modifying function, invoked below.
-     const dispatch = useDispatch()
-     const textC = props.item + ':' + parseFloat(setting).toFixed(1)
-     return (
-          <div className= {styles.itemwrapper}>
-               <div className={styles.wordwrapper}>
-                    {textC}
-               </div>
-               <div className = {styles.buttonwrapper}>
-                    <ToggleButton color = "inherit" variant = "outlined" size = "small"
-                      onClick={() => dispatch(props.decrement())}>-</ToggleButton>
-                    <ToggleButton color = "inherit" variant = "outlined" size = "small"
-                      onClick={() => dispatch(props.increment())}>+</ToggleButton>
-              </div>
-          </div>
-     );
+  const setting = useSelector(props.setting) // Get current value of the setting.
+  // useDispatch returns the state modifying function, invoked below.
+  const dispatch = useDispatch()
+  const textC = props.item + ':' + parseFloat(setting).toFixed(1)
+  return (
+    <div className={styles.itemwrapper}>
+      <div className={styles.wordwrapper}>
+        {textC}
+      </div>
+      <div className={styles.buttonwrapper}>
+        <ToggleButton color="inherit" variant="outlined" size="small"
+          onClick={() => dispatch(props.decrement())}>-</ToggleButton>
+        <ToggleButton color="inherit" variant="outlined" size="small"
+          onClick={() => dispatch(props.increment())}>+</ToggleButton>
+      </div>
+    </div>
+  );
 }

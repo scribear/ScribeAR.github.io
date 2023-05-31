fee244eb39d89e955181f1463704e3de5145d657
JoniLi99 <qiyuanl2@illinois.edu>
2023-05-31 00:54:14 -0500

    Update phraseListAdd.tsx

--- src/components/navbars/sidebar/phrase/phraseListAdd.tsx
+++ src/components/navbars/sidebar/phrase/phraseListAdd.tsx
@@ -5,6 +5,8 @@ import PhrasePopUp from './phraseList';
 import './phrase.css'
 import { RootState, PhraseListStatus, PhraseList } from '../../../../react-redux&middleware/redux/typesImports';
 import { useDispatch, useSelector } from 'react-redux';
+import withReactContent from 'sweetalert2-react-content';
+import axios from 'axios';
 
 /* todo:
   1   Make language bar a fixed height so that it can only display ~8 languages 
@@ -13,7 +15,10 @@ import { useDispatch, useSelector } from 'react-redux';
       and save the shortened title in RootState
 */
 
-export default function CustomizedMenus() {
+// Initialize SweetAlert2
+const MySwal = withReactContent(Swal);
+
+export default function PhraseListAdd(props) {
   const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const phraseListStatus = useSelector((state: RootState) => {
@@ -51,23 +56,61 @@ export default function CustomizedMenus() {
       (event: React.KeyboardEvent | React.MouseEvent) => {
         setState({ ...state, shouldCollapse: !state.shouldCollapse })
       }
-  const clickAddList = () => {
-    Swal.fire({
-      customClass: {
-        container: 'swal2-container',
-      },
-      text: "Make a title for your list of phrases",
-      input: 'text',
-      returnFocus: true,
-      showCancelButton: true
-    }).then((result) => {
-      if (result.value) {
-        initialPhraseList.name = result.value
-        dispatch({ type: 'ADD_PHRASE_LIST', payload: initialPhraseList })
-        dispatch({ type: 'CHANGE_LIST', payload: initialPhraseList.phrases })
+
+  const clickAddList = async () => {
+    const { value: formValues } = await MySwal.fire({
+      title: 'Input GitHub Information',
+      html:
+        '<input id="swal-input1" class="swal2-input" placeholder="File Name">' +
+        '<input id="swal-input2" class="swal2-input" placeholder="GitHub Owner">' +
+        '<input id="swal-input3" class="swal2-input" placeholder="Repository">' +
+        '<input id="swal-input4" class="swal2-input" placeholder="Branch">' +
+        '<input id="swal-input5" class="swal2-input" placeholder="GitHub Token">',
+      focusConfirm: false,
+      preConfirm: () => {
+        return [
+          document.getElementById('swal-input1').value,
+          document.getElementById('swal-input2').value,
+          document.getElementById('swal-input3').value,
+          document.getElementById('swal-input4').value,
+          document.getElementById('swal-input5').value,
+        ]
       }
-    });
-  }
+    })
+
+    if (formValues) {
+      const [fileName, owner, repo, branch, token] = formValues;
+
+      // Fetch file from GitHub
+      const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contents/${fileName}?ref=${branch}`, {
+        headers: { 'Authorization': `token ${token}` },
+      });
+
+      const fileContent = atob(response.data.content);
+      const words = fileContent.split('\n');
+
+      const newList = {name: fileName, phrases: words, availableSpace: 10 - words.length}
+      dispatch({ type: 'ADD_PHRASE_LIST', payload: newList })
+      dispatch({ type: 'CHANGE_LIST', payload: newList.phrases })
+    }
+
+  // const clickAddList = () => {
+  //   Swal.fire({
+  //     customClass: {
+  //       container: 'swal2-container',
+  //     },
+  //     text: "Make a title for your list of phrases",
+  //     input: 'text',
+  //     returnFocus: true,
+  //     showCancelButton: true
+  //   }).then((result) => {
+  //     if (result.value) {
+  //       initialPhraseList.name = result.value
+  //       dispatch({ type: 'ADD_PHRASE_LIST', payload: initialPhraseList })
+  //       dispatch({ type: 'CHANGE_LIST', payload: initialPhraseList.phrases })
+  //     }
+  //   });
+  // }
 
   return (
     <div>
@@ -94,5 +137,3 @@ export default function CustomizedMenus() {
     </div>
   );
 }
-
-

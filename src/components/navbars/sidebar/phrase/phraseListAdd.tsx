956818e3263de5bf97230021c6bb67f8230b03b7
JoniLi99 <qiyuanl2@illinois.edu>
2023-05-31 09:54:06 -0500

    Update phraseListAdd.tsx

--- src/components/navbars/sidebar/phrase/phraseListAdd.tsx
+++ src/components/navbars/sidebar/phrase/phraseListAdd.tsx
@@ -5,8 +5,6 @@ import PhrasePopUp from './phraseList';
 import './phrase.css'
 import { RootState, PhraseListStatus, PhraseList } from '../../../../react-redux&middleware/redux/typesImports';
 import { useDispatch, useSelector } from 'react-redux';
-import withReactContent from 'sweetalert2-react-content';
-import axios from 'axios';
 
 /* todo:
   1   Make language bar a fixed height so that it can only display ~8 languages 
@@ -15,10 +13,7 @@ import axios from 'axios';
       and save the shortened title in RootState
 */
 
-// Initialize SweetAlert2
-const MySwal = withReactContent(Swal);
-
-export default function PhraseListAdd(props) {
+export default function CustomizedMenus() {
   const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const phraseListStatus = useSelector((state: RootState) => {
@@ -56,61 +51,23 @@ export default function PhraseListAdd(props) {
       (event: React.KeyboardEvent | React.MouseEvent) => {
         setState({ ...state, shouldCollapse: !state.shouldCollapse })
       }
-
-  const clickAddList = async () => {
-    const { value: formValues } = await MySwal.fire({
-      title: 'Input GitHub Information',
-      html:
-        '<input id="swal-input1" class="swal2-input" placeholder="File Name">' +
-        '<input id="swal-input2" class="swal2-input" placeholder="GitHub Owner">' +
-        '<input id="swal-input3" class="swal2-input" placeholder="Repository">' +
-        '<input id="swal-input4" class="swal2-input" placeholder="Branch">' +
-        '<input id="swal-input5" class="swal2-input" placeholder="GitHub Token">',
-      focusConfirm: false,
-      preConfirm: () => {
-        return [
-          (document.getElementById('swal-input1') as HTMLInputElement)?.value || '',
-          (document.getElementById('swal-input2') as HTMLInputElement)?.value || '',
-          (document.getElementById('swal-input3') as HTMLInputElement)?.value || '',
-          (document.getElementById('swal-input4') as HTMLInputElement)?.value || '',
-          (document.getElementById('swal-input5') as HTMLInputElement)?.value || '',          
-        ]
+  const clickAddList = () => {
+    Swal.fire({
+      customClass: {
+        container: 'swal2-container',
+      },
+      text: "Make a title for your list of phrases",
+      input: 'text',
+      returnFocus: true,
+      showCancelButton: true
+    }).then((result) => {
+      if (result.value) {
+        initialPhraseList.name = result.value
+        dispatch({ type: 'ADD_PHRASE_LIST', payload: initialPhraseList })
+        dispatch({ type: 'CHANGE_LIST', payload: initialPhraseList.phrases })
       }
-    })
-
-    if (formValues) {
-      const [fileName, owner, repo, branch, token] = formValues;
-
-      // Fetch file from GitHub
-      const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contents/${fileName}?ref=${branch}`, {
-        headers: { 'Authorization': `token ${token}` },
-      });
-
-      const fileContent = atob(response.data.content);
-      const words = fileContent.split('\n');
-
-      const newList = {name: fileName, phrases: words, availableSpace: 10 - words.length}
-      dispatch({ type: 'ADD_PHRASE_LIST', payload: newList })
-      dispatch({ type: 'CHANGE_LIST', payload: newList.phrases })
-    }
-
-  // const clickAddList = () => {
-  //   Swal.fire({
-  //     customClass: {
-  //       container: 'swal2-container',
-  //     },
-  //     text: "Make a title for your list of phrases",
-  //     input: 'text',
-  //     returnFocus: true,
-  //     showCancelButton: true
-  //   }).then((result) => {
-  //     if (result.value) {
-  //       initialPhraseList.name = result.value
-  //       dispatch({ type: 'ADD_PHRASE_LIST', payload: initialPhraseList })
-  //       dispatch({ type: 'CHANGE_LIST', payload: initialPhraseList.phrases })
-  //     }
-  //   });
-  // }
+    });
+  }
 
   return (
     <div>

b0d8b733af5580df9b00d1d37b41fe4d64801e90
Guru Nanma P <42802604+18nanma@users.noreply.github.com>
2023-07-09 16:47:56 -0500

    update whisper iframe visibility to none

--- src/mode/WhisperFrame.tsx
+++ src/mode/WhisperFrame.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const WhisperFrame: React.FC = () => {
   return (
-    <iframe src="https://scribear.illinois.edu/v/whisper/" />
+    <iframe 
+      src="https://scribear.illinois.edu/v/whisper/" 
+      style={{display: "none"}}
+    />
   );
 };
 

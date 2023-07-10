27eda7fab1e01e3785938e4c2f4e579066a2be0b
Guru Nanma P <42802604+18nanma@users.noreply.github.com>
2023-07-09 23:08:20 -0500

    Update to show download of model completed

--- src/components/navbars/topbar/api/WhisperDropdown.tsx
+++ src/components/navbars/topbar/api/WhisperDropdown.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { List, ListItem, Button } from '../../../../muiImports'
 
+
 export default function WhisperDropdown(props) {
 
     // const [isTrue, setIsTrue] = useState<boolean>(false);
   // const [isClearCache, setIsClearCache] = useState<boolean>(false);
   const [isDownloadTiny, setIsDownloadTiny] = useState<boolean>(false);
   const [isDownloadBase, setIsDownloadBase] = useState<boolean>(false);
+  const [progress, setProgress] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
 
     const handleTinyDownload = () => {
@@ -19,7 +22,43 @@ export default function WhisperDropdown(props) {
         console.log("clicked download tiny")
         setIsDownloadBase(prevIsDownloadBase => !prevIsDownloadBase);
         sessionStorage.setItem('isDownloadBase', (!isDownloadBase).toString());
+
+        const intervalId = setInterval(() => {
+          let p = sessionStorage.getItem('whisper_progress');
+          if (p === '100%') {
+              alert("Base model downloaded! trigger the mike by stopping and starting again");
+              clearInterval(intervalId); // Stop checking once it's 100%
+          }
+      }, 2000); // Check every 2000 milliseconds, or 2 seconds
       };
+
+      // --------------- Using a modal to show the progress
+
+      // const handleBaseDownload = () => {
+      //   console.log("clicked download base");
+      //   setIsDownloadBase(prevIsDownloadBase => !prevIsDownloadBase);
+      //   sessionStorage.setItem('isDownloadBase', (!isDownloadBase).toString());
+    
+      //   // Initially show the modal
+      //   setShowModal(true);
+    
+      //   const intervalId = setInterval(() => {
+      //     let p = sessionStorage.getItem('whisper_progress');
+      //     setProgress(p); // Update the progress
+      //     if (p === '100%') {
+      //       clearInterval(intervalId); // Stop checking once it's 100%
+      //     }
+      //   }, 2000);
+      // };
+    
+      // // Close the modal when progress reaches 100%
+      // useEffect(() => {
+      //   if (progress === '100%') {
+      //     setShowModal(false);
+      //   }
+      // }, [progress]);
+
+     
 
       // const handleClickClearCache = () => {
       //   console.log("clicked cache")
@@ -27,7 +66,9 @@ export default function WhisperDropdown(props) {
       //   sessionStorage.setItem('isClearCache', (!isClearCache).toString());
       // };
 
+
     return (
+      <>
         <div>
             <List component="div" disablePadding>
                <ListItem sx={{ pl: 4 }} style={{ width: '100%' }}>
@@ -42,5 +83,6 @@ export default function WhisperDropdown(props) {
                </ListItem> */}
             </List>
         </div>
+        </>
     )
 }

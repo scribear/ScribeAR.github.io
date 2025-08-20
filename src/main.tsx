50a19b56a71745bf9cf8ea48101a69b8a71dc267
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2025-08-20 11:20:18 -0700

    better fix for typescript error

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-const root = document.getElementById('root');
-if (root) {
-  createRoot(root).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-} else {
-  throw new Error(
-    'Missing root element! Ensure element with id="root" exists in index.html!',
-  );
-}
+createRoot(document.getElementById('root') as Element).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);

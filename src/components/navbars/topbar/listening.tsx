0ddbf70f4f547e3e4e710395d06121ad71d7c7e2
vikiyarema <vikiyaremaa@gmail.com>
2025-10-19 22:06:29 -0500

    added tool tip for "menu" and added a keyboard shortcut: ctrl m

--- src/components/navbars/topbar/listening.tsx
+++ src/components/navbars/topbar/listening.tsx
@@ -4,37 +4,42 @@ import { RootState } from '../../../store';
 import { ControlStatus } from '../../../react-redux&middleware/redux/typesImports';
 import Theme from '../../theme'
 import * as React from 'react';
-
+import { useHotkeys } from 'react-hotkeys-hook'
 
    export default function CustomizedMenus(props) {
     console.log(Theme())
      const dispatch = useDispatch()
+
      
      let controlStatus = useSelector((state: RootState) => {
           return state.ControlReducer as ControlStatus;
      });
 
-     const handleClick = (event: React.KeyboardEvent | React.MouseEvent) => {
+     const handleClick = (event: React.KeyboardEvent | React.MouseEvent | KeyboardEvent) => {
           controlStatus.listening = !controlStatus.listening
           dispatch({type: 'FLIP_RECORDING', payload: controlStatus})
-     }
+     } 
+
      const {myTheme} = Theme()
+     
+     useHotkeys('ctrl+m', (e) => {
+          handleClick(e)
+     })
 
      // color "primary" comes from Theme()
      return (
-       <div>
-        <ThemeProvider theme={myTheme}>
-          <Tooltip title = 
-            {props.listening === false ? "Begin Listening" : "Pause Listening"}>
-            <IconButton color="primary" onClick={handleClick}>
-              {props.listening ? 
-                <MicIcon color="primary" fontSize="large"/> : 
-                <MicOffIcon color="primary" fontSize="large"/>
-              }
-            </IconButton>
-          </Tooltip>
-         </ThemeProvider>
-
-       </div>
+        <div>
+            <ThemeProvider theme={myTheme}>
+                    <Tooltip title = 
+                        {props.listening === false ? "Begin Listening" : "Pause Listening"}>
+                        <IconButton color="primary" onClick={handleClick}>
+                            {props.listening ? 
+                                <MicIcon color="primary" fontSize="large"/> : 
+                                <MicOffIcon color="primary" fontSize="large"/>
+                            }
+                        </IconButton>
+                    </Tooltip>
+            </ThemeProvider>
+        </div>
      );
    }
